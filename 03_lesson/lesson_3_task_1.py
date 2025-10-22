1fdb467e3625b077df411118a2d4e12e3f2092d0
EkaGash89 <eka590@vk.com>
2025-10-22 19:23:25 +0500

    3

--- 03_lesson/lesson_3_task_1.py
+++ 03_lesson/lesson_3_task_1.py
@@ -2,6 +2,6 @@ from user import User
 
 my_user = User("Петр", "Петров")
 
-print(my_user.f_name)
-print(my_user.l_name)
+print(my_user.self.f_name)
+print(my_user.self.l_name)
 print(my_user.get_product_info())
