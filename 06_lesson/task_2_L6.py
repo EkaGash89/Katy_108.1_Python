ead1408a08e6eec6d5a6916bd50a1a561703ff20
EkaGash89 <eka590@vk.com>
2025-11-16 16:10:01 +0500

    6

---
+++ 06_lesson/task_2_L6.py
@@ -0,0 +1,26 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
+
+#2) Переименовать кнопку
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.implicitly_wait(20)
+
+driver.maximize_window()
+driver.get("http://uitestingplayground.com/textinput") #переход на сайт
+pole_voda = driver.find_element(By.CSS_SELECTOR, '#newButtonName').send_keys("SkyPro")
+
+button = driver.find_element(By.CSS_SELECTOR, '#updatingButton').click()
+
+button_text = driver.find_element(By.CSS_SELECTOR, '#updatingButton').text
+    
+print(button_text)  # Выведет "SkyPr
+
+driver.quit()
+
+
+
+
