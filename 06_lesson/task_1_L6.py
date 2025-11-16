5b7b868f6d87865080e3450ae6d9993335681be5
EkaGash89 <eka590@vk.com>
2025-11-16 17:44:17 +0500

    6

--- 06_lesson/task_1_L6.py
+++ 06_lesson/task_1_L6.py
@@ -1,9 +1,7 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Keys
 
 #1) Нажать на кнопку
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
